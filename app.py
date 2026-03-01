6b8d0da332403565ccf995613dec9489762ec670
DelhaizeHalle <lukas.laporte@hotmail.com>
2026-03-01 01:22:28 +0100

    Update app.py

--- app.py
+++ app.py
@@ -259,7 +259,7 @@ elif menu == "📦 Product analyse (PRO)":
 ]
 
 # Enkel Too Good To Go
-    tg2g = tg2g[tg2g["reden"].str.lower() == "verlies andere"]
+    tg2g = tg2g[tg2g["reden"].str.lower().str.strip() == "verlies andere"]
 
 # 🔥 AANTAL pakketten
     aantal_pakketten = tg2g["stuks"].sum()
@@ -494,6 +494,7 @@ elif menu == "📤 Upload":
 
             except Exception as e:
                 st.error(f"❌ Upload fout: {e}")
+
 
 
 
