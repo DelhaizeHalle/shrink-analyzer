60525ca4cfb31d13d08e8072649dc4f4a0d27398
DelhaizeHalle <lukas.laporte@hotmail.com>
2026-03-04 11:03:36 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import streamlit as st
 import pandas as pd
 from supabase import create_client
 import datetime
@@ -418,6 +419,7 @@ elif menu == "➕ Data invoeren":
 
         st.success(f"✅ Opgeslagen voor {afdeling}")
         st.cache_data.clear()
+
 
 
 
