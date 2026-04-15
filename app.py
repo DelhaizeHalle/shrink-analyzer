09d18f442d1904137b716b66efbe1225cacadb25
DelhaizeHalle <lukas.laporte@hotmail.com>
2026-04-15 10:48:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1118,7 +1118,7 @@ elif menu == "🧊 Demo promoties":
         on=["hope", "week"],
         how="left"
     )
-
+    df_merge["product"] = df_merge["product_x"]
     df_merge["euro"] = pd.to_numeric(df_merge["euro"], errors="coerce").fillna(0)
 
     # =====================
