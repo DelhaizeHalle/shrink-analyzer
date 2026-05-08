90169cc649b9e9111e50baf525fcf68701e6b2f1
DelhaizeHalle <lukas.laporte@hotmail.com>
2026-05-08 12:27:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -459,7 +459,8 @@ if menu == "📊 Dashboard":
     col4.metric(
         "📉 vs vorige week",
         format_euro(current),
-        format_euro(delta)
+        format_euro(delta),
+        delta_color="inverse"  # 🔥 DIT IS DE FIX
     )
 
   
